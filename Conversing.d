09b7ddd4cd1c89bc3039554a1a7c33aae1f64bf6
Vaptistis Anogeianakis <nomad@cornercase.gr>
2012-11-18 00:09:30 +0200

    Moved parsing code outside of main and into a separate function.
    --HG--
    branch : refactoring

--- Conversing.d
+++ Conversing.d
@@ -1,35 +1,19 @@
-import std.stdio, std.string, std.array, std.algorithm;
+import std.stdio, std.string;
 import Expressions;
 
 
 void main()
 {
 	immutable string[] operators = ["+","-","*","/","%"];
-	Expression[] stack;
 	
 	writeln("Type in a line of text and press enter. (Type ctrl+z and enter to exit)");
 	write("<< ");
 	
 	foreach(line; stdin.byLine())
 	{
-		// parse input line
-		foreach(token; std.array.splitter(line))
-		{
-			if(find(operators,token)==[]) // not an operator
-			{
-				stack ~= new LiteralOperand(token.idup);
-			}
-			else
-			{
-				auto right = stack.back();
-				stack.popBack();
-				auto left = stack.back();
-				stack.popBack();
-				stack ~= new BinaryOperator(token.idup,left,right);
-			} // end else
-		} // end foreach
 		// print output line
-		writeln(">> ",stack.back().serialize());
+		auto ast = parseExpression(line,operators);
+		writeln(">> ",ast?ast.serialize():"error: stack not exhausted!");
 		// prepare for new input
 		write("<< ");
 	} // end foreach
--- Expressions.d
+++ Expressions.d
@@ -1,4 +1,4 @@
-import std.stdio, std.string;
+import std.stdio, std.string, std.algorithm, std.array;
 
 interface Expression
 {
@@ -46,3 +46,28 @@ unittest
 		new BinaryOperator("-",new LiteralOperand("b"),new LiteralOperand("2.2")))).serialize() ==
 		"((5 * a) + (b - 2.2))");
 }
+
+Expression parseExpression(char[] input, immutable string[] operators)
+{
+	Expression[] stack;
+
+	foreach(token; std.array.splitter(input))
+	{
+		if(find(operators,token)==[]) // not an operator
+		{
+			stack ~= new LiteralOperand(token.idup);
+		}
+		else // an operator
+		{
+			auto right = stack.back();
+			stack.popBack();
+			auto left = stack.back();
+			stack.popBack();
+			stack ~= new BinaryOperator(token.idup,left,right);
+		} // end else
+	} // end foreach
+	if(stack.length == 1)
+		return stack.back();
+	else
+		return null;
+} // end function parseExpression
