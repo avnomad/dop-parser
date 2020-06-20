ddc18c81b1bb664ce8b6e1f97b835be656cb3743
Vaptistis Anogeianakis <nomad@cornercase.gr>
2020-06-20 03:36:03 +0300

    Added 155 new unit tests for the parseInfixExpression function.
    Those covered negative tests with distfix and confix operators but without
    juxtaposition, prefix, infix or postfix operators and no operands. Cases with
    1-3 tokens were covered.
    
    --HG--
    branch : testing

--- source/dopParser.d
+++ source/dopParser.d
@@ -1782,6 +1782,104 @@ unittest // negative tests with juxtaposition
 	)(test_cases ~ common_test_cases);
 } // end unittest
 
+unittest // negative tests with distfix and confix operators and without juxtaposition
+{
+	immutable infix_operators = ["+":Op(5, Assoc.left), "-":Op(5, Assoc.left),
+								"*":Op(6, Assoc.left), "~": Op(7, Assoc.right)];
+	immutable prefix_operators = ["$":10, "-":10, "%":10, "~":10];
+	immutable postfix_operators = ["!":11, "*":11, "%":11, "~":11];
+
+	immutable paren = Tup("(", ",", ")");
+	immutable bracket = Tup("[", ",", "]");
+	Tup[string] initiators = ["(":paren, "[":bracket];
+	immutable separators = [",":paren, ",":bracket];
+	Tup[string] terminators = [")":paren, "]":bracket];
+
+	immutable test_cases = [
+		// no prefix, infix or postfix operators. no operands
+		// one tokens
+		["(", confix_imbalance], [",", missing_separator_left], [")", confix_imbalance],
+		["[", confix_imbalance], ["]", confix_imbalance],
+		// two tokens
+		["( (", confix_imbalance      ], [", (", missing_separator_left], [") (", confix_imbalance],
+		["( ,", missing_separator_left], [", ,", missing_separator_left], [") ,", confix_imbalance],
+		["( )", ""/* not an error */  ], [", )", missing_separator_left], [") )", confix_imbalance],
+		["( [", confix_imbalance      ], [", [", missing_separator_left], [") [", confix_imbalance],
+		["( ]", confix_imbalance      ], [", ]", missing_separator_left], [") ]", confix_imbalance],
+
+		["[ (", confix_imbalance      ], ["] (", confix_imbalance      ],
+		["[ ,", missing_separator_left], ["] ,", confix_imbalance      ],
+		["[ )", confix_imbalance      ], ["] )", confix_imbalance      ],
+		["[ [", confix_imbalance      ], ["] [", confix_imbalance      ],
+		["[ ]", ""/* not an error */  ], ["] ]", confix_imbalance      ],
+
+		// three tokens
+		["( ( (", confix_imbalance      ], ["( , (", missing_separator_left ], ["( ) (", no_op_no_juxt_init     ],
+		["( ( ,", missing_separator_left], ["( , ,", missing_separator_left ], ["( ) ,", missing_separator_right],
+		["( ( )", confix_imbalance      ], ["( , )", missing_separator_left ], ["( ) )", confix_imbalance       ],
+		["( ( [", confix_imbalance      ], ["( , [", missing_separator_left ], ["( ) [", no_op_no_juxt_init     ],
+		["( ( ]", confix_imbalance      ], ["( , ]", missing_separator_left ], ["( ) ]", confix_imbalance       ],
+
+		["( [ (", confix_imbalance      ], ["( ] (", confix_imbalance       ],
+		["( [ ,", missing_separator_left], ["( ] ,", confix_imbalance       ],
+		["( [ )", confix_imbalance      ], ["( ] )", confix_imbalance       ],
+		["( [ [", confix_imbalance      ], ["( ] [", confix_imbalance       ],
+		["( [ ]", confix_imbalance      ], ["( ] ]", confix_imbalance       ],
+
+		[", ( (", missing_separator_left], [", , (", missing_separator_left ], [", ) (", missing_separator_left],
+		[", ( ,", missing_separator_left], [", , ,", missing_separator_left ], [", ) ,", missing_separator_left],
+		[", ( )", missing_separator_left], [", , )", missing_separator_left ], [", ) )", missing_separator_left],
+		[", ( [", missing_separator_left], [", , [", missing_separator_left ], [", ) [", missing_separator_left],
+		[", ( ]", missing_separator_left], [", , ]", missing_separator_left ], [", ) ]", missing_separator_left],
+
+		[", [ (", missing_separator_left], [", ] (", missing_separator_left ],
+		[", [ ,", missing_separator_left], [", ] ,", missing_separator_left ],
+		[", [ )", missing_separator_left], [", ] )", missing_separator_left ],
+		[", [ [", missing_separator_left], [", ] [", missing_separator_left ],
+		[", [ ]", missing_separator_left], [", ] ]", missing_separator_left ],
+
+		[") ( (", confix_imbalance      ], [") , (", confix_imbalance       ], [") ) (", confix_imbalance      ],
+		[") ( ,", confix_imbalance      ], [") , ,", confix_imbalance       ], [") ) ,", confix_imbalance      ],
+		[") ( )", confix_imbalance      ], [") , )", confix_imbalance       ], [") ) )", confix_imbalance      ],
+		[") ( [", confix_imbalance      ], [") , [", confix_imbalance       ], [") ) [", confix_imbalance      ],
+		[") ( ]", confix_imbalance      ], [") , ]", confix_imbalance       ], [") ) ]", confix_imbalance      ],
+
+		[") [ (", confix_imbalance      ], [") ] (", confix_imbalance       ],
+		[") [ ,", confix_imbalance      ], [") ] ,", confix_imbalance       ],
+		[") [ )", confix_imbalance      ], [") ] )", confix_imbalance       ],
+		[") [ [", confix_imbalance      ], [") ] [", confix_imbalance       ],
+		[") [ ]", confix_imbalance      ], [") ] ]", confix_imbalance       ],
+
+		["[ ( (", confix_imbalance      ], ["[ , (", missing_separator_left ], ["[ ) (", confix_imbalance       ],
+		["[ ( ,", missing_separator_left], ["[ , ,", missing_separator_left ], ["[ ) ,", confix_imbalance       ],
+		["[ ( )", confix_imbalance      ], ["[ , )", missing_separator_left ], ["[ ) )", confix_imbalance       ],
+		["[ ( [", confix_imbalance      ], ["[ , [", missing_separator_left ], ["[ ) [", confix_imbalance       ],
+		["[ ( ]", confix_imbalance      ], ["[ , ]", missing_separator_left ], ["[ ) ]", confix_imbalance       ],
+
+		["[ [ (", confix_imbalance      ], ["[ ] (", no_op_no_juxt_init     ],
+		["[ [ ,", missing_separator_left], ["[ ] ,", missing_separator_right],
+		["[ [ )", confix_imbalance      ], ["[ ] )", confix_imbalance       ],
+		["[ [ [", confix_imbalance      ], ["[ ] [", no_op_no_juxt_init     ],
+		["[ [ ]", confix_imbalance      ], ["[ ] ]", confix_imbalance       ],
+
+		["] ( (", confix_imbalance      ], ["] , (", confix_imbalance       ], ["] ) (", confix_imbalance      ],
+		["] ( ,", confix_imbalance      ], ["] , ,", confix_imbalance       ], ["] ) ,", confix_imbalance      ],
+		["] ( )", confix_imbalance      ], ["] , )", confix_imbalance       ], ["] ) )", confix_imbalance      ],
+		["] ( [", confix_imbalance      ], ["] , [", confix_imbalance       ], ["] ) [", confix_imbalance      ],
+		["] ( ]", confix_imbalance      ], ["] , ]", confix_imbalance       ], ["] ) ]", confix_imbalance      ],
+
+		["] [ (", confix_imbalance      ], ["] ] (", confix_imbalance       ],
+		["] [ ,", confix_imbalance      ], ["] ] ,", confix_imbalance       ],
+		["] [ )", confix_imbalance      ], ["] ] )", confix_imbalance       ],
+		["] [ [", confix_imbalance      ], ["] ] [", confix_imbalance       ],
+		["] [ ]", confix_imbalance      ], ["] ] ]", confix_imbalance       ],
+	];
+
+	runUnitTests!(test_input => collectExceptionMsg(parseInfixExpression(infix_operators, prefix_operators,
+													postfix_operators, initiators, separators, terminators, test_input))
+	)(test_cases);
+} // end unittest
+
 mixin template Named()
 {
 	string name;
