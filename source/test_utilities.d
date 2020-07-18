9d5cbb6f612ef61be7a0f60e124adddc8c5514f6
Vaptistis Anogeianakis <nomad@cornercase.gr>
2020-07-18 06:02:25 +0300

    Modified copyright notice to make it compatible with DDoc.
    [D's Documentation Generator](https://dlang.org/spec/ddoc.html) defines a
    special `Copyright:` section for documenting `module` declarations. Adopting the
    specified syntax will enable tools to extract the copyright notice and include
    it in generated documentation for the files.
    
    There are a number of different options for generating documentation from D
    source files that aim to be compatible with the above specification and all have
    slight differences in how they handle certain constructs:
    
    - DDoc built into dmd.
      Used with `dub build --build=docs`.
      Wouldn't pick up the copyright section by default.
    - [DDOX](https://github.com/rejectedsoftware/ddox).
      Used with `dub build --build=ddox`.
      Reflows text and removes links in "<...>".
    - [harbored-mod](https://github.com/dlang-community/harbored-mod).
      Installed with `dub fetch harbored-mod; dub build harbored-mod`.
      Used with `path/to/hmod .`.
    
    The `copyright` section in D documentation comments is only picked up when there
    is a `module` declaration, so I added one to all remaining files.
    
    Also replaced some tabs inside the copyright notices with single spaces.
    
    --HG--
    branch : documentation

--- source/test_utilities.d
+++ source/test_utilities.d
@@ -1,20 +1,23 @@
-//	Copyright (C) 2012-2013, 2018, 2020 Vaptistis Anogeianakis <nomad@cornercase.gr>
-/*
- *	This file is part of DOP Parser.
+/**
+ * Copyright: (C) 2012-2013, 2018, 2020 Vaptistis Anogeianakis <nomad@cornercase.gr>
  *
- *	DOP Parser is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ * This file is part of DOP Parser.
  *
- *	DOP Parser is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ * DOP Parser is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with DOP Parser.  If not, see <http://www.gnu.org/licenses/>.
+ * DOP Parser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DOP Parser.  If not, see <http://www.gnu.org/licenses/>.
  */
+module test_utilities;
+
 import std.stdio, std.string, std.datetime.stopwatch, std.datetime.systime, std.range.primitives, std.traits;
 
 // Test case numbering is one-based.
